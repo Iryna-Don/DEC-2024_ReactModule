368ea22416265ab3f5c231f8d194a9df1623b476
Iryna-Don <idonets0104081518@gmail.com>
2025-03-21 11:57:17 +0100

    destructured the value object

--- src/components/CharacterComponent.tsx
+++ src/components/CharacterComponent.tsx
@@ -7,12 +7,13 @@ interface ISimpsonPersonProp {
 }
 
 const CharacterComponent:FC<ISimpsonPersonProp> = ({value}) => {
+    const {name, surname, age, info, photo} = value;
     return (
         <div className={styles.characterCard}>
-            <h2>{value.name} {value.surname}</h2>
-            <p><b>Age:</b> {value.age}</p>
-            <p><b>Info:</b> {value.info}</p>
-            <img src={value.photo} alt={`${value.name} ${value.surname}`}/>
+            <h2>{name} {surname}</h2>
+            <p><b>Age:</b> {age}</p>
+            <p><b>Info:</b> {info}</p>
+            <img src={photo} alt={`${name} ${surname}`}/>
         </div>
     );
 };
