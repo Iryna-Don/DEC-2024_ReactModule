7bba36affb0d051e88296063e6b40611b88293b1
Iryna-Don <idonets0104081518@gmail.com>
2025-03-21 09:21:15 +0100

    added hw_#Fs05Kiho second version (other prop transfer)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
+// import {coursesAndDurationArray} from './data.ts';
+// import './App.css'
+// import CourseWithDuration from "./components/CourseWithDuration.tsx";
+//
+//
+// function App()  {
+//     return (
+//         <>
+//             <div>
+//                 {coursesAndDurationArray.map((course, index) => <CourseWithDuration key={index} title={course.title} monthDuration={course.monthDuration}/>)
+//                 }        </div>
+//
+//         </>
+//     )
+// }
+//
+// export default App
+
 import {coursesAndDurationArray} from './data.ts';
 import './App.css'
 import CourseWithDuration from "./components/CourseWithDuration.tsx";
 
 
-function App()  {
+function App() {
     return (
         <>
             <div>
-                {coursesAndDurationArray.map((course, index) => <CourseWithDuration key={index} title={course.title} monthDuration={course.monthDuration}/>)
-                }        </div>
+                {coursesAndDurationArray.map((course, index) => <CourseWithDuration key={index} value={course}/>)}
+            </div>
 
         </>
     )
--- src/components/CourseWithDuration.tsx
+++ src/components/CourseWithDuration.tsx
@@ -1,11 +1,28 @@
+// import {ICourseAndDuration} from '../data.ts';
+// import {FC} from "react";
+//
+// const CourseWithDuration: FC<ICourseAndDuration> = ({title, monthDuration}) => {
+//     return (
+//         <>
+//             <p>{title}</p>
+//             <p>{monthDuration}</p>
+//         </>
+//     );
+// };
+//
+// export default CourseWithDuration;
+
 import {ICourseAndDuration} from '../data.ts';
 import {FC} from "react";
+interface CourseWithDurationProps {
+    value: ICourseAndDuration;
+}
 
-const CourseWithDuration: FC<ICourseAndDuration> = ({title, monthDuration}) => {
+const CourseWithDuration: FC<CourseWithDurationProps> = ({value}) => {
     return (
         <>
-            <p>{title}</p>
-            <p>{monthDuration}</p>
+            <p>{value.title}</p>
+            <p>{value.monthDuration}</p>
         </>
     );
 };
