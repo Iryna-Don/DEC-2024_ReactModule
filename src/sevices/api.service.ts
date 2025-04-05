512ad60026d599f3c871947686c262fec68dfdc6
Iryna-Don <idonets0104081518@gmail.com>
2025-04-05 10:14:23 +0200

    `ve taken info from https://developer.themoviedb.org/docs/getting-started WITHOUT STRUCTURE

--- src/sevices/api.service.ts
+++ src/sevices/api.service.ts
@@ -1,22 +1,39 @@
 import axios from 'axios';
 import {IMovieResponse} from "../models/IMovieResponse.ts";
+
 const apiKey = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5YTVlZmUxZTA5MzdjZTE0YmE4ZDA1YTliMjRmNzQxOCIsIm5iZiI6MTcyMzU4MTU0OS45NzcsInN1YiI6IjY2YmJjNDZkZDhjN2YwODYxMmQwMzIzNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KMXiPLHS7rwvdSTtkw7lwCIEkqPaNiaIh5vw3OJdm3k';
 const baseUrl = 'https://api.themoviedb.org/3';
 export const baseImgUrl = 'https://image.tmdb.org/t/p/w200/';
 
+const axiosInstance = axios.create(({
+    baseURL: baseUrl,
+    headers: {
+        'accept': 'application/json'
+    },
+}))
+
+axiosInstance.interceptors.request.use((request) => {
+    request.headers.set(
+        {
+            ...request.headers,
+            'Authorization': `Bearer ${apiKey}`,
+        }
+    )
+    request.params = {
+        ...request.params,
+        include_adult: 'false',
+        include_video: 'false',
+        language: 'en-US',
+        sort_by: 'popularity.desc'
+    };
+    return request;
+});
+
 
 export const getMovies = async (page: number = 1): Promise<IMovieResponse> => {
-    const response = await axios.get(`${baseUrl}/discover/movie`, {
-        headers: {
-            Authorization: `Bearer ${apiKey}`,
-            accept: 'application/json'
-        },
+    const response = await axiosInstance.get(`/discover/movie`, {
         params: {
-            include_adult: 'false',
-            include_video: 'false',
-            language: 'en-US',
             page: page,
-            sort_by: 'popularity.desc'
         },
     });
     return response.data;
@@ -24,34 +41,18 @@ export const getMovies = async (page: number = 1): Promise<IMovieResponse> => {
 
 
 export const getMovieByID = async (id: number): Promise<any> => {
-    const response = await axios.get(`${baseUrl}/find/${id}`,
-         {
-        headers: {
-            Authorization: `Bearer ${apiKey}`,
-            accept: 'application/json'
-        },
-    });
+    const response = await axiosInstance.get(`/movie/${id}`)
     return response.data;
 }
 
-
-export const searchData = async (search:string): Promise<IMovieResponse> => {
-    const response = await axios.get(`${baseUrl}/search/movie?query=${search}`,
-         {
-        headers: {
-            Authorization: `Bearer ${apiKey}`,
-            accept: 'application/json'
-        },
-    });
+export const searchData = async (search: string): Promise<IMovieResponse> => {
+    const response = await axiosInstance.get(`/search/movie?query=${search}`)
     return response.data;
 }
 
 export const getMovieByGenre = async (genreID: number): Promise<IMovieResponse> => {
-    const response = await axios.get(`${baseUrl}/discover/movie`,
-        { headers: {
-                Authorization: `Bearer ${apiKey}`,
-                accept: 'application/json'
-            },
+    const response = await axiosInstance.get(`/discover/movie`,
+        {
             params: {
                 with_genres: genreID,
             },
@@ -59,4 +60,3 @@ export const getMovieByGenre = async (genreID: number): Promise<IMovieResponse> 
         });
     return response.data;
 }
-
